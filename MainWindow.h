54f6bac90a37bd80e4b8399f436186c0907bdd2d
CyberKuryber <urosstanic25@gmail.com>
2022-04-20 22:30:55 +0200

    +main window

---
+++ MainWindow.h
@@ -0,0 +1,39 @@
+//
+// Created by uros on 20/04/22.
+//
+
+#ifndef PARALLEL_REGRESSION_MAINWINDOW_H
+#define PARALLEL_REGRESSION_MAINWINDOW_H
+#include "QMainWindow"
+#include "QPushButton"
+#include "QString"
+#include "QGridLayout"
+#include "QSpinBox"
+#include "QLabel"
+
+class MainWindow: public QMainWindow {
+    Q_OBJECT
+public:
+     MainWindow(const QString &title);
+//     ~MainWindow();
+
+private:
+    QLabel *label_x_min;
+    QLabel *label_x_max;
+    QLabel *label_points;
+    QLabel *label_x_err;
+    QLabel *label_y_err;
+
+    QSpinBox *input_x_min;
+    QSpinBox *input_x_max;
+    QSpinBox *input_points;
+    QSpinBox *input_x_err;
+    QSpinBox *input_y_err;
+    QPushButton *button_serial;
+    QPushButton *button_for;
+    QPushButton *button_task;
+
+};
+
+
+#endif //PARALLEL_REGRESSION_MAINWINDOW_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
 #include <QApplication>
-#include <QPushButton>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button_serial("Serial Least Squares", nullptr);
-    QPushButton button_for("For Least Squares", nullptr);
-    QPushButton button_task("Task Least Squares", nullptr);
-    button_serial.resize(100,50);
-    button_for.resize(100,50);
-    button_task.resize(100,50);
-    button_serial.show();
-    button_for.show();
-    button_task.show();
+    MainWindow mainWindow("Main Window");
+    mainWindow.show();
     return QApplication::exec();
 }
